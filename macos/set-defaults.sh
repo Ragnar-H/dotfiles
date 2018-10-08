cd35933e4b01466ac975e34ed1cb4b3628df7b31
Ragnar-H <ragnarhardarson90@gmail.com>
2018-10-08 16:03:21 +0200

    Faster key repeat rate

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -28,8 +28,8 @@ defaults write com.apple.AppleMultitouchTrackpadTrackpadRightClick -int 0
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Set a faster keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Set the timezone; see `sudo systemsetup -listtimezones` for other values
 sudo systemsetup -settimezone "Europe/Copenhagen" > /dev/null
